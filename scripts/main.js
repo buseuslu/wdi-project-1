bca1faf97ab01bec44d248d9cb1b64559aa2b29d
Buse Uslu <buseuslu24@gmail.com>
2019-02-20 23:23:05 +0000

    added aliens and moved left and right

--- scripts/main.js
+++ scripts/main.js
@@ -22,29 +22,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const squareElement = document.querySelectorAll('.square')
   squareElement[1].classList.add('alien')
-  squareElement[2].classList.add('alien')
   squareElement[3].classList.add('alien')
-  squareElement[4].classList.add('alien')
   squareElement[5].classList.add('alien')
-  squareElement[6].classList.add('alien')
   squareElement[7].classList.add('alien')
-  squareElement[8].classList.add('alien')
-  squareElement[11].classList.add('alien')
   squareElement[12].classList.add('alien')
-  squareElement[13].classList.add('alien')
   squareElement[14].classList.add('alien')
-  squareElement[15].classList.add('alien')
   squareElement[16].classList.add('alien')
-  squareElement[17].classList.add('alien')
   squareElement[18].classList.add('alien')
   squareElement[21].classList.add('alien')
-  squareElement[22].classList.add('alien')
   squareElement[23].classList.add('alien')
-  squareElement[24].classList.add('alien')
   squareElement[25].classList.add('alien')
-  squareElement[26].classList.add('alien')
   squareElement[27].classList.add('alien')
-  squareElement[28].classList.add('alien')
+
 
   squareElement[ship.position].classList.add('ship')
 
@@ -70,57 +59,56 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // move aliens down
   const alien = {
-    position: [1 ,2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28]
+    position: [1, 3, 5, 7, 12, 14, 16, 18, 21, 23, 25, 27]
   }
 
-  function moveAliens() {
-    if (gameOver) {
-      return
-    }
-    const aliens = document.querySelectorAll('.alien')
-    for(let x = 0; x < aliens.length; x++) {
-      if (alien.position[x] > 79) {
-        gameOver = true
-        // alert('game over')
-        return
-      }
-
-      squareElement[alien.position[x]].classList.remove('alien')
-      alien.position[x] = alien.position[x] + 10
-      squareElement[alien.position[x]].classList.add('alien')
-    }
-  }
-  // // move aliens left and right
+  // function moveAliens() {
+  //   if (gameOver) {
+  //     return
+  //   }
+  //   const aliens = document.querySelectorAll('.alien')
+  //   for(let x = 0; x < aliens.length; x++) {
+  //     if (alien.position[x] > 79) {
+  //       gameOver = true
+  //       // alert('game over')
+  //       return
+  //     }
+  //
+  //     squareElement[alien.position[x]].classList.remove('alien')
+  //     alien.position[x] = alien.position[x] + 10
+  //     squareElement[alien.position[x]].classList.add('alien')
+  //   }
+  // }
+  // move aliens left and right
   // const alienPosition = alien.position
   // const aliens = document.querySelectorAll('.alien')
-  // let movesMade = 0
-  // setInterval(() => {
-  //   movesMade++
-  //   for(let i = 0; i < aliens.length; i++) {
-  //     if (movesMade === 10) {
-  //       squareElement[alien.position[i]].classList.remove('alien')
-  //       alien.position[i] = alien.position[i] + 10
-  //       squareElement[alien.position[i]].classList.add('alien')
-  //       alien.position.push(alienPosition)
-  //       movesMade = 0
-  //     } else if (movesMade === 5) {
-  //       squareElement[alien.position[i]].classList.remove('alien')
-  //       alien.position[i] = alien.position[i] + 10
-  //       squareElement[alien.position[i]].classList.add('alien')
-  //       alien.position.push(alienPosition)
-  //     } else if (movesMade > 5 && movesMade < 2) {
-  //       squareElement[alien.position[i]].classList.remove('alien')
-  //       alien.position[i] = alien.position[i] - 1
-  //       squareElement[alien.position[i]].classList.add('alien')
-  //       alien.position.push(alienPosition)
-  //     } else if(movesMade < 5) {
-  //       squareElement[alien.position[i]].classList.remove('alien')
-  //       alien.position[i] = alien.position[i] + 1
-  //       squareElement[alien.position[i]].classList.add('alien')
-  //       alien.position.push(alienPosition)
-  //     }
-  //   }
-  // }, 1000)
+  let movesMade = 0
+  setInterval(() => {
+    for(let i = 0; i < alien.position.length; i++) {
+      if (movesMade === 3) {
+        squareElement[alien.position[i]].classList.remove('alien')
+        alien.position[i] = alien.position[i] + 10
+        squareElement[alien.position[i]].classList.add('alien')
+      } else if (movesMade === 1) {
+        squareElement[alien.position[i]].classList.remove('alien')
+        alien.position[i] = alien.position[i] + 10
+        squareElement[alien.position[i]].classList.add('alien')
+      } else if (movesMade > 1 && movesMade < 4) {
+        squareElement[alien.position[i]].classList.remove('alien')
+        alien.position[i] = alien.position[i] - 1
+        squareElement[alien.position[i]].classList.add('alien')
+      } else if(movesMade < 1) {
+        squareElement[alien.position[i]].classList.remove('alien')
+        alien.position[i] = alien.position[i] + 1
+        squareElement[alien.position[i]].classList.add('alien')
+      }
+    }
+    if (movesMade === 3) {
+      movesMade = 0
+    } else {
+      movesMade++
+    }
+  }, 1000)
 
 
   //fire missile
@@ -175,17 +163,17 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   //change screens
   //start screen
-  $('.start-screen').innerHTML = $('start-screen')
-  $('#starbutton').click(function() {
-    $('start-screen').parent().css('hidden')
-  })
+  // $('.start-screen').innerHTML = $('start-screen')
+  // $('#starbutton').click(function() {
+  //   $('start-screen').parent().css('hidden')
+  // })
 
   //game-over + total score screen
 
   let gameOver = false
   window.addEventListener('keydown', moveShip)
   window.addEventListener('keydown', fireMissile)
-  window.setInterval(moveAliens, 5000)
-  window.setInterval(moveMissile, 500)
+  // window.setInterval(moveAliens, 5000)
+  window.setInterval(moveMissile, 300)
 
 })
